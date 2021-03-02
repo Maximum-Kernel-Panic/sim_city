f8135882bfb2c8d65349313f7bb0ad1e6e1c0bc5
Maximum-kernel-panic <erik.blae@outlook.com>
2021-03-02 15:57:02 +0100

    added second wrapper

---
+++ assignment3/Explicit_Problem_2t1.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 14:07:52 2021
+
+@author: erik
+"""
+
+import numpy as np
+from assimulo.ode import *
+#from assimulo.solvers import CVode
+from assimulo.explicit_ode import Explicit_ODE
+#import matplotlib.pyplot as mpl
+
+class Explicit_Problem_2_to_1wrap():
+    
+    def __init__(self, rhs, t0, y0, y0d):
+        self.rhs2nd = rhs
+        self.y0 = y0
+        self.y0d = y0d
+        self.t0 = t0
+        self.state0 = np.concatenate([y0,y0d])
+        self.dim = len(y0)
+        #print(self.dim)
+        
+    
+    #translates the right-hand-side function to first order
+    def buffRHS(self, t, x):
+        xpos = x[0:self.dim]
+        v = x[self.dim:2*self.dim]
+        vdot = self.rhs2nd(t,xpos,v)
+        ret = np.concatenate([v,vdot])
+        return ret
+      
+
+    #makes the Explicit_Problem model to be passed to a solver
+    def makeModel(self):
+        
+        model = Explicit_Problem(self.buffRHS,self.state0,self.t0)
+        return model
+        
+
+    
+
+def rhs1(t, y, v):
+    
+    k = 100
+    lmd = k*(np.sqrt(y[0]*y[0] + y[1]*y[1])-1)/np.sqrt(y[0]*y[0] + y[1]*y[1])
+    y2d = -y[1]*lmd-1
+    y1d = -y[0]*lmd
+    xdot = np.array([y1d,y2d])
+    return xdot
+
+def rhs2(t,y):
+    k = 100
+    lmd = k*(np.sqrt(y[0]*y[0] + y[1]*y[1])-1)/np.sqrt(y[0]*y[0] + y[1]*y[1])
+    y4d = -y[1]*lmd-1
+    y3d = -y[0]*lmd
+    y2d = y[3]
+    y1d = y[2]
+    
+    yd = np.array([y1d, y2d, y3d, y4d])
+    return yd
+
+
+'''
+
+s2 = 1/np.sqrt(2)
+x0 = np.array([-s2+0.05,-s2+0.05])
+x0d = np.array([0,0])
+
+testprob = Explicit_Problem_2t1wrap(rhs1,0,x0,x0d)
+#model2= Explicit_Problem(rhs2,x0,0)
+
+#print(testprob.dim)
+#print(testprob.buffRHS(0,x0))
+
+model = testprob.makeModel()
+
+
+sim = CVode(model)
+#sim2 = CVode(model2)
+print(model.rhs(0,[1,0]))
+
+t,y = sim.simulate(10)
+#t2, y2 = sim2.simulate(10)
+mpl.plot(t,y[:,0])
+mpl.plot(t,y[:,1])
+'''
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ assignment3/Explicit_Problem_MCKt1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 14:01:03 2021
+
+@author: erik
+"""
+
+import numpy as np
+import Explicit_Problem_2t1 as c2t1
+from assimulo.ode import *
+from scipy.linalg import inv
+from assimulo.solvers import CVode
+from assimulo.explicit_ode import Explicit_ODE
+import matplotlib.pyplot as mpl
+
+class Explicit_Problem_MCK_to_1wrap():
+    
+    def __init__(self, M, C, K, F, t0, x0, x0d):
+        
+        self.M = M
+        self.Mm1 = inv(M)
+        self.C = C
+        self.K = K
+        self.F = F
+        self.t0 = t0
+        self.x0 = x0
+        self.x0d = x0d
+        
+        
+    #converts problem from MCK form to second order 
+    def convRHS(self, t, x, xd):
+        Kt = self.K(t,x,xd)
+        xdd = self.Mm1@(self.F(t,x)-self.C@np.transpose(xd)-Kt@np.transpose(x))
+        return xdd
+    
+    def makeModel(self):
+        converter = c2t1.Explicit_Problem_2_to_1wrap(self.convRHS,self.t0,self.x0,self.x0d)
+        model = converter.makeModel()
+        return model
+    
+    
+
+'''
+Kt = np.eye(2)
+Ct = np.array([[0.1,0 ],[0, 0.1]])
+Mt = np.eye(2)
+x0 = np.array([1.5,-1.5])
+x0d = np.array([0,0])
+
+def F(t,x):
+    return np.array([0,0])
+
+def Kf(t,x,xd):
+    #return np.eye(2)
+    return np.array([[1/np.sqrt(abs(x[0])), 0.7],[-0.1, 1/np.sqrt(abs(x[1]))]])
+
+converter = Explicit_Problem_MCK_to_1wrap(Mt, Ct, Kf, F, 0, x0, x0d)
+print(converter.convRHS(0,x0,x0d))
+model = converter.makeModel()
+
+
+
+sim = CVode(model)
+t,y = sim.simulate(10)
+mpl.plot(t,y[:,0])
+mpl.plot(t,y[:,1])
+mpl.grid()
+'''
